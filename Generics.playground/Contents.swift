b44182ff7c5ec72344fff67711959653f3e48448
Dhikshith <dhikshith@folksable.com>
2023-09-18 03:55:17 +0530

    Generics

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -89,3 +89,159 @@ var bar: Float = 20
 
 foo <-> bar
 print("foo is \(foo), bar is \(bar)") // "foo is 20.0, bar is 10.0"
+
+protocol CanJump {
+    func jump()
+}
+
+protocol CanRun {
+    func run()
+}
+
+struct Person: CanJump, CanRun {
+    func jump() {
+        print("Jumping...")
+    }
+    
+    func run() {
+        print("Running...")
+    }
+}
+
+func jumpAndRun<T: CanJump & CanRun>(_ value: T) {
+    value.jump();
+    value.run()
+}
+
+let person = Person();
+
+jumpAndRun(person)
+
+let names: Array<String> = ["Foo", "Bar"]
+
+// new syntax
+extension [String] {
+    func findLongestString() -> String? {
+        var longest: String?=nil;
+        
+        forEach({a in
+            if(a.count >= (longest?.count ?? 0)){
+                longest = a;
+            }
+        })
+        
+        return longest;
+    }
+}
+
+
+[
+    "Foo",
+    "Bar Baz",
+    "Qux"
+].findLongestString()
+
+protocol View {
+    func addSubView(_ view: View)
+}
+
+extension View {
+    func addSubView(_ view: View) {
+        // empty
+    }
+}
+
+struct Button: View {
+    
+}
+
+struct Table: View {
+    
+}
+
+
+
+protocol PresentableAsView {
+    associatedtype ViewType: View
+    func produceView() -> ViewType
+    func configure(
+        superView: View,
+        thisView: ViewType
+    )
+    func present(
+        view: ViewType,
+        on superView: View
+    )
+}
+
+
+extension PresentableAsView {
+    func configure(
+        superView: View,
+        thisView: ViewType
+    ) {
+        // empty by default
+    }
+    
+    func present(
+        view: ViewType,
+        on superView: View
+    ) {
+        superView.addSubView(view)
+    }
+}
+
+struct MyButton: PresentableAsView {
+    func produceView() -> Button {
+        Button()
+    }
+    func configure(superView: View, thisView: Button) {
+        
+    }
+    func present(view: Button, on superView: View) {
+        
+    }
+}
+
+
+extension PresentableAsView {
+    func doSomethingWithView() {
+        "This is a View"
+    }
+}
+
+let button = MyButton()
+button.doSomethingWithView();
+
+
+struct MyTable: PresentableAsView {
+    func produceView() -> Table {
+        Table()
+    }
+    
+}
+
+let myTable = MyTable()
+
+myTable.doSomethingWithView()
+
+extension [Int] {
+    func averate() -> Double {
+//        self.reduce(0.0){ (prev: Double, this: Int) -> Double in
+//            prev + Double(this)/Double(count);
+//        }
+        Double(self.reduce(0, +))/Double(count);
+    }
+}
+
+let myitems = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7
+]
+
+print(myitems.averate())
