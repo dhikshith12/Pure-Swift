80bedb5d6a137cb2ff0f5b91bc51d0ee4195c3c1
Dhikshith <dhikshith@folksable.com>
2023-09-17 22:07:21 +0530

    closures

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -12,3 +12,36 @@ add = { (lhs: Int, rhs: Int) -> Int in
 
 add(10, 50)
 
+func customAdd(
+    _ lhs: Int,
+    _ rhs: Int,
+    using function: (Int, Int) -> Int
+) -> Int {
+    function(lhs, rhs);
+}
+
+customAdd(
+    10,
+    50
+) { lhs, rhs in
+    lhs+rhs;
+}
+
+customAdd(
+    10,
+    50
+) {
+    $0 + $1;
+}
+
+customAdd(10, 50) { $0 + $1 }
+
+let ages = [ 30, 20, 19, 40 ]
+
+let res = ages.sorted(by: <)
+
+print(res)
+
+
+
+
